be31e425af2ffeafb0f2ec2433b6794f40b2de9f
Praveen Sahu <praveen@ladsolutions.in>
2022-10-21 05:00:36 +0530

    303_Custom_Filters

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var app = angular.module("myApp", []);
 app.controller("mcon", function ($scope,$filter) {
-  $scope.mydata = { arr: ["mike", "jake", "test"] };
   $scope.mydata2 = {
     arr: [
       { name: "test", age: 34, class: "highschool" },
@@ -8,5 +7,35 @@ app.controller("mcon", function ($scope,$filter) {
       { name: "user5", age: 21, class: "middleschool" },
     ],
   };
-  console.log($filter('uppercase')('testing'));;
 });
+app.filter('charlimit',function() {
+    return function(input, length) {
+        if (!length) {
+            length=10;
+        }
+        if (!input) {
+            return "";
+        }
+        if (input.length<=length) {
+            return input;
+        }else{
+            return input.substring(0,length)+'....';
+        }
+    }
+})
+
+app.filter('candrink',function() {
+    return function(data,minage) {
+        var filtered = [];
+        if (!minage) {
+            minage=21;
+        }
+        for (let i = 0; i < data.length; i++) {
+            const value = data[i];
+            if (value.age>=minage) {
+                filtered.push(value);
+            }
+        }
+        return filtered;
+    }
+})
