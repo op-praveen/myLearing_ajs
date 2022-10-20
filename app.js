f3694085ba125b987b4e9b7912a57ad93abf00f7
Praveen Sahu <praveen@ladsolutions.in>
2022-10-21 04:45:24 +0530

    301 Built-In Filters and 302 trick with filter

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var app = angular.module("myApp", []);
-app.controller("mcon", function ($scope) {
-    $scope.mydata = {arr:["mike","jake","test"]}
-    $scope.mydata2 = {arr:[{name:"test",age:34,class:'highschool'}]};
+app.controller("mcon", function ($scope,$filter) {
+  $scope.mydata = { arr: ["mike", "jake", "test"] };
+  $scope.mydata2 = {
+    arr: [
+      { name: "test", age: 34, class: "highschool" },
+      { name: "user2", age: 22, class: "middleschool" },
+      { name: "user5", age: 21, class: "middleschool" },
+    ],
+  };
+  console.log($filter('uppercase')('testing'));;
 });
