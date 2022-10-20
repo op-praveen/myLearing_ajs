be9786c6683e5e3921974a4c5fd71f52ccb462dd
Praveen Sahu <praveen@ladsolutions.in>
2022-10-20 17:43:56 +0530

    working on ngClass, ng-template and ng-include

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 angular.module("myApp", []).controller("mcon", function ($scope) {
-    $scope.myArr2 = ['A','b','c','d','e','f'];
-    $scope.users = [{'name':'test','status':'active'}];
+    $scope.classVar = "red";
 });
