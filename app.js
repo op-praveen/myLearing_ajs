3c343121e60d59c96a553d4583bcd4185392a11f
Praveen Sahu <praveen@ladsolutions.in>
2022-10-21 04:37:35 +0530

    301 Built-In Filters

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var app = angular.module("myApp", []);
 app.controller("mcon", function ($scope) {
-    $scope.mydata= 'this is data';
+    $scope.mydata = {arr:["mike","jake","test"]}
+    $scope.mydata2 = {arr:[{name:"test",age:34,class:'highschool'}]};
 });
