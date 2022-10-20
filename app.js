1877ccb89fcaea92be282b132480ac57e8323670
Praveen Sahu <praveen@ladsolutions.in>
2022-10-20 19:05:02 +0530

    learning ngCloak

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 var app = angular.module("myApp", []);
 app.controller("mcon", function ($scope) {
-    $scope.num=0;
-    $scope.nums=[];
-    $scope.increment= function() {
-        $scope.num++;
-    }
-    $scope.breakit=$scope.$watch('num',function() {
-        $scope.nums.push($scope.num)
-    })
+    $scope.mydata= 'this is data';
 });
