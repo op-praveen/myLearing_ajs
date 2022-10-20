8b605d8295d5b0c6fcd7e8eeb7d92f53fbe86ce2
Praveen Sahu <praveen@ladsolutions.in>
2022-10-20 18:23:11 +0530

    learn how we use  in app

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 var app = angular.module("myApp", []);
-app.controller("mcon", function ($scope) {});
-app.controller("sub-ctrl", function ($scope) {});
+app.controller("mcon", function ($scope) {
+    $scope.count= 1;
+    $scope.$watch('mydata.val',function(newVal) {
+        // console.log('yea');
+        $scope.count += 1;
+        console.log($scope.count);
+        if (newVal) {
+            $scope.mydata_toolong = newVal.length > 15;
+        }
+    });
+});
