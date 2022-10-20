d46e4c457d1ce3023968669cec50184318baf4ff
Praveen Sahu <praveen@ladsolutions.in>
2022-10-20 17:07:26 +0530

    0203 Built-In Directives

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-(function(){
-    "use strict";
-    angular.module("myApp",[])
-    .controller("mcon",function($scope) {
-        $scope.name="test";
-    });
-})();
+angular.module("myApp", []).controller("mcon", function ($scope) {
+  $scope.val2 = 1;
+  $scope.even = false;
+  $scope.test = function () {
+    $scope.val2 += 1;
+    $scope.even = $scope.val2 % 2 == 0;
+  };
+});
