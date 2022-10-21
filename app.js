e4e75a725e7cf5684f29b7640b634f78b023eda8
Praveen Sahu <praveen@ladsolutions.in>
2022-10-21 11:57:10 +0530

    ajs constant and values services

--- app.js
+++ app.js
@@ -1,41 +1,7 @@
 var app = angular.module("myApp", []);
-app.controller("mcon", function ($scope,$filter) {
-  $scope.mydata2 = {
-    arr: [
-      { name: "test", age: 34, class: "highschool" },
-      { name: "user2", age: 22, class: "middleschool" },
-      { name: "user5", age: 21, class: "middleschool" },
-    ],
-  };
+app.controller("mcon", function ($scope,twitterApi, valService) {
+    // twitterApi.url="test";
+    console.log(twitterApi.url,valService);
 });
-app.filter('charlimit',function() {
-    return function(input, length) {
-        if (!length) {
-            length=10;
-        }
-        if (!input) {
-            return "";
-        }
-        if (input.length<=length) {
-            return input;
-        }else{
-            return input.substring(0,length)+'....';
-        }
-    }
-})
-
-app.filter('candrink',function() {
-    return function(data,minage) {
-        var filtered = [];
-        if (!minage) {
-            minage=21;
-        }
-        for (let i = 0; i < data.length; i++) {
-            const value = data[i];
-            if (value.age>=minage) {
-                filtered.push(value);
-            }
-        }
-        return filtered;
-    }
-})
+app.constant("twitterApi",{url:"api.twitter.com"});
+app.value("valService",function() {return "this is return from a fn"});
